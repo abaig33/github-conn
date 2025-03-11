b0973f7a0fe45b9140bd1413c179f33c3b8b99bf
AB <147617576+abaig33@users.noreply.github.com>
2025-03-11 23:17:06 +0530

    code-deploy-final

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abaig33/simple-python-flask-app
+docker pull arshanbaig/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abaig33/simple-python-flask-app
+docker run -d -p 5000:5000 arshanbaig/simple-python-flask-app
+
